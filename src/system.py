744e9b13b300560c30acc9d8e9fcf50b7dde3c44
mdforbes500 <mforbes4@gmu.edu>
2019-03-01 02:35:04 -0500

    Added some preliminary work on the system class.

--- src/system.py
+++ src/system.py
@@ -1,6 +1,26 @@
 #! /usr/bin/python
+import random
 
 class System:
 
     def __init__(self):
-        #stuff
+
+        self.jump_shadow = random.randint(1,6)
+        self.orbits = self.jump_shadow + random.randint(1,6)
+
+        #Determine if gas giants are present
+        if random.randint(2,12) < 10:
+            self.gas_giants = True
+        else:
+            self.gas_giants = False
+        rolls = [None None]
+
+        #Determine Inner, Habitable, and Outer zones
+        for roll in rolls:
+            roll = random.randint(1,6)
+        if rolls[0] <= rolls[1]:
+            lower = rolls[0]
+            upper = rolls[1]
+        else:
+            lower = rolls[1]
+            upper = rolls[0]
