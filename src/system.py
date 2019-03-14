a3c720af64f981a0d4012f82c617fbadefa8e928
mdforbes500 <mforbes4@gmu.edu>
2019-03-14 18:49:50 -0400

    updated.

--- src/system.py
+++ src/system.py
@@ -9,18 +9,30 @@ class System:
         self.orbits = self.jump_shadow + random.randint(1,6)
 
         #Determine if gas giants are present
-        if random.randint(2,12) < 10:
-            self.gas_giants = True
-        else:
-            self.gas_giants = False
-        rolls = [None None]
+        self.gas_giats = 0
+        running = True
+        counter = 0
+        while running:
+            if random.randint(2,12) < (10 - counter):
+                self.gas_giants = self.gas_giants + 1
+                counter = counter + 1
+            else:
+                running = False
 
         #Determine Inner, Habitable, and Outer zones
-        for roll in rolls:
-            roll = random.randint(1,6)
-        if rolls[0] <= rolls[1]:
-            lower = rolls[0]
-            upper = rolls[1]
+        roll_one = random.randint(1,6)
+        roll_two = random.randint(1,6)
+        if roll_one <= roll_two:
+            lower = roll_one
+            upper = roll_two
         else:
-            lower = rolls[1]
-            upper = rolls[0]
+            lower = roll_two
+            upper = roll_one
+
+        self.inner = [None]*lower
+        if (self.orbits - upper) >= 0:
+            self.outer = [None]*(self.orbits - upper)
+        else:
+            self.outer = []
+        if  (upper - lower) > 1 and ():
+            self.habitable = [None]*(upper - lower)
